de18fa887a83625c6a5fa80a43655b493c437d66
captainTigerYT <raunakmanna43@gmail.com>
2024-08-15 13:34:36 +0530

    added a basic post detail page

---
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -0,0 +1,104 @@
+import { validateRequest } from "@/auth";
+import FollowButton from "@/components/FollowButton";
+import Linkify from "@/components/Linkify";
+import Post from "@/components/posts/post";
+import UserAvatar from "@/components/userAvatar";
+import UserTooltip from "@/components/UserTooltip";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude, UserData } from "@/lib/types";
+import { Loader2 } from "lucide-react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import { cache, Suspense } from "react";
+
+interface PageProps {
+  params: { postId: string };
+}
+
+const getPost = cache(async (postId: string, loggedInUserId: string) => {
+  const post = await prisma.post.findUnique({
+    where: { id: postId },
+    include: getPostDataInclude(loggedInUserId),
+  });
+
+  if (!post) notFound();
+
+  return post;
+});
+
+export async function generateMetadata({ params: { postId } }: PageProps) {
+  const { user } = await validateRequest();
+  if (!user) return {};
+  const post = await getPost(postId, user.id);
+  return {
+    title: `${post.user.displayName}:${post.content.slice(0, 50)}...`,
+  };
+}
+
+export default async function Page({ params: { postId } }: PageProps) {
+  const { user } = await validateRequest();
+
+  if (!user)
+    return (
+      <p className="text-destructive">You are not allowed to do such sorcery</p>
+    );
+
+  const post = await getPost(postId, user.id);
+
+  return (
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <Post post={post} />
+      </div>
+      <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
+        <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
+          <UserInfoSidebar user={post.user} />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
+
+interface UserInfoSidebarProps {
+  user: UserData;
+}
+
+async function UserInfoSidebar({ user }: UserInfoSidebarProps) {
+  const { user: loggedInUser } = await validateRequest();
+  if (!loggedInUser) return null;
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">This guy?</div>
+      <UserTooltip user={user}>
+        <Link
+          href={`/users/${user.username}`}
+          className="flex items-center gap-3"
+        >
+          <UserAvatar avatarUrl={user.avatarUrl} className="flex-none" />
+          <div>
+            <p className="line-clamp-1 break-all font-semibold hover:underline">
+              {user.displayName}
+            </p>
+            <p className="line-clamp-1 break-all text-muted-foreground">
+              @{user.username}
+            </p>
+          </div>
+        </Link>
+      </UserTooltip>
+      <Linkify>
+        <div className="line-clamp-6 whitespace-pre-line break-words to-muted-foreground">
+          {user.bio}
+        </div>
+      </Linkify>
+      {user.id !== loggedInUser.id && (
+        <FollowButton userId={user.id} initialState={{
+          followers: user._count.followers,
+          isFollowedByUser: user.followers.some(
+            ({ followerId }) => followerId === loggedInUser.id,
+          ),
+        }} />
+      )}
+    </div>
+  );
+}
--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { PostData } from "@/lib/types";
 import Link from "next/link";
 import UserAvatar from "../userAvatar";
@@ -26,34 +27,41 @@ export default function Posts({ post }: PostsProps) {
 
   return (
     <article className="group/post space-y-3 rounded-2xl bg-card p-5 shadow-[0_3px_15px_rgb(0,0,0,0.12)]">
-      <div className="flex justify-between gap-3">
-        <div className="flex flex-wrap gap-2">
-          <UserTooltip user={post.user}>
-            <Link href={`/users/${post.user.username}`} className="flex gap-1">
-              <UserAvatar avatarUrl={post.user.avatarUrl} />
+      <Link href={`/posts/${post.id}`} suppressHydrationWarning>
+        <div className="flex justify-between gap-3">
+          <div className="flex flex-wrap gap-2">
+            <UserTooltip user={post.user}>
+              <div className="flex gap-1">
+                <UserAvatar avatarUrl={post.user.avatarUrl} />
 
-              <div>
-                <span className="flex items-center gap-1 font-medium hover:underline">
-                  {post.user.displayName}
-                  {badge(post.user) == "OP" && <Crown size={17} color="gold" />}
-                  {badge(post.user) == "Verified" && (
-                    <CircleCheck size={17} color="#1F75FE" />
-                  )}
-                </span>
-                <span className="block text-xs text-muted-foreground">
-                  {formatReletiveDate(post.createdAt)}
-                </span>
+                <div>
+                  <span className="flex items-center gap-1 font-medium hover:underline">
+                    {post.user.displayName}
+                    {badge(post.user) == "OP" && (
+                      <Crown size={17} color="gold" />
+                    )}
+                    {badge(post.user) == "Verified" && (
+                      <CircleCheck size={17} color="#1F75FE" />
+                    )}
+                  </span>
+                  <span
+                    className="block text-xs text-muted-foreground"
+                    suppressHydrationWarning
+                  >
+                    {formatReletiveDate(post.createdAt)}
+                  </span>
+                </div>
               </div>
-            </Link>
-          </UserTooltip>
+            </UserTooltip>
+          </div>
+          {post.user.id === user?.id && (
+            <PostMoreButton
+              post={post}
+              className="opacity-0 transition-opacity group-hover/post:opacity-100"
+            />
+          )}
         </div>
-        {post.user.id === user?.id && (
-          <PostMoreButton
-            post={post}
-            className="opacity-0 transition-opacity group-hover/post:opacity-100"
-          />
-        )}
-      </div>
+      </Link>
       <Linkify>
         <div className="whitespace-pre-line break-words">{post.content}</div>
       </Linkify>
